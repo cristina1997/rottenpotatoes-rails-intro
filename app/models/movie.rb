f2a4c87547911431c71393d23867f0e7cc1b18a4
cristina1997 <cristinaflorinaeu@gmail.com>
2019-02-21 16:21:19 +0000

    Added comments to movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
     
     def self.ratings
+        # obtain all the distinct ratings from the movies
         Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating}
     end
 end
